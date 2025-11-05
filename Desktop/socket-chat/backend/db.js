ce63478ed44cb37f96ce61b598d00d6adcb6acf4
Aleksandra500 <aleksandracomagic7@gmail.com>
2025-11-06 00:05:36 +0100

    connection with AlwaysData

--- Desktop/socket-chat/backend/db.js
+++ Desktop/socket-chat/backend/db.js
@@ -1,3 +1,19 @@
 const mysql = require('mysql2')
 require('dotenv').config()
 
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+ 
+})
+ 
+
+db.connect(err => {
+    if(err) console.log('DB nije povezan ❌', err);
+    else console.log('DB je uspesno povezan ✅');
+})
+
+
+module.exports=db;
--- Desktop/socket-chat/backend/index.js
+++ Desktop/socket-chat/backend/index.js
@@ -1,6 +1,26 @@
-require('dotenv').config()
-const express = require('express')
-const http = require('http')
-const {server} = require('socket.io')
-const mysql = require('mysql2')
+require('dotenv').config();
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const db = require('./db');
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
+io.on('connection', (socket) => {
+	console.log('A user connected: ', socket.id);
+
+	socket.on('chatMessage', (msg) => {
+		console.log('Message received: ', msg);
+		io.emit('chatMessage', msg);
+	});
+
+	socket.on('disconnect', () => {
+		console.log('User disconnect: ', socket.id);
+	});
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () =>
+	console.log(`Server je pokrenut na ${PORT} `)
+);
