c0e1da32a26b9ecd5001c635cf99b2350b827ee0
Aleksandra500 <aleksandracomagic7@gmail.com>
2025-11-12 09:29:25 +0100

    PWA plugin

--- Desktop/socket-chat/frontend/vite.config.js
+++ Desktop/socket-chat/frontend/vite.config.js
@@ -1,11 +1,36 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import { VitePWA } from 'vite-plugin-pwa';
+
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [
+  plugins: [
     react(),
     tailwindcss(),
-  
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Socket Chat',
+        short_name: 'ChatApp',
+        description: 'Real-time chat app with sockets',
+        theme_color: '#0d6efd',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
 });
